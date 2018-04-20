47df5f393fd018590348e2b0131d5c029b672fe7
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-04-20 12:33:42 +0300

    first version of quick sort for numbers

---
+++ quick-sort.js
@@ -0,0 +1,40 @@
+const array = [5, 6, 1, 9, 3, 7, 2, 8, 4]
+
+const quickSort = (array, sortedArray = []) => {
+  const pivot = array[Math.floor(Math.random()*array.length)]
+  let lowerValuesArray = []
+  let higherValuesArray = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= pivot) {
+      higherValuesArray.push(array[i])
+    } else {
+      lowerValuesArray.push(array[i])
+    }
+  }
+  if (lowerValuesArray.length == 1) {
+    sortedArray.push(lowerValuesArray[0])
+  } else if (lowerValuesArray.length > 1){
+    quickSort(lowerValuesArray, sortedArray) 
+  }
+
+  if (higherValuesArray.length == 1) {
+    sortedArray.push(higherValuesArray[0])
+  } else if (higherValuesArray.length > 1){
+    quickSort(higherValuesArray, sortedArray)
+  }
+  return sortedArray
+}
+
+Array.prototype.quickSort = function() {
+  return quickSort(this)
+}
+
+console.time("quickSort")
+const newArray = array.quickSort()
+console.log(newArray);
+console.timeEnd("quickSort")
+
+// console.time("a")
+// array.sort((a, b) => a - b)
+// console.log('js sort function', array);
+// console.timeEnd("a")
