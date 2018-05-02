6accb76aa3538cf8409b40bcc58e74814ba90499
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-05-02 08:45:09 +0300

    changed quick-sort

--- quick-sort.js
+++ quick-sort.js
@@ -1,27 +1,28 @@
-const array = [5, 6, 1, 9, 3, 7, 2, 8, 4]
+const array = [11, 5, 6, 10, 1, 9, 14, 17, 18, 20, 25, 3, 7, 2, 8, 4, 13]
 
 const quickSort = (array, sortedArray = []) => {
+  if (array.length <= 2) {
+    if (array[0] > array[1]) {
+      array[1] && sortedArray.push(array[1])
+      array[0] && sortedArray.push(array[0])
+    } else {
+      array[0] && sortedArray.push(array[0])
+      array[1] && sortedArray.push(array[1])
+    }
+    return sortedArray
+  }
   const pivot = array[Math.floor(Math.random()*array.length)]
-  let lowerValuesArray = []
-  let higherValuesArray = []
+  let l = [] //left part of array
+  let r = [] // right part of array
   for (let i = 0; i < array.length; i++) {
     if (array[i] >= pivot) {
-      higherValuesArray.push(array[i])
+      r.push(array[i])
     } else {
-      lowerValuesArray.push(array[i])
+      l.push(array[i])
     }
   }
-  if (lowerValuesArray.length == 1) {
-    sortedArray.push(lowerValuesArray[0])
-  } else if (lowerValuesArray.length > 1){
-    quickSort(lowerValuesArray, sortedArray) 
-  }
-
-  if (higherValuesArray.length == 1) {
-    sortedArray.push(higherValuesArray[0])
-  } else if (higherValuesArray.length > 1){
-    quickSort(higherValuesArray, sortedArray)
-  }
+  quickSort(l, sortedArray) 
+  quickSort(r, sortedArray)
   return sortedArray
 }
 
@@ -29,6 +30,8 @@ Array.prototype.quickSort = function() {
   return quickSort(this)
 }
 
-
+console.time("quickSort")
 const newArray = array.quickSort()
-console.log(newArray)
+console.log(newArray);
+console.timeEnd("quickSort")
+
